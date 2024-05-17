9c92c098d923a29bd233f1119c6270ac6b32b1ef
shaibu lawal yusuf <58580624+ahbiggie@users.noreply.github.com>
2024-05-17 14:43:13 +0100

    Add files via upload
    after adding sounds in js

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 var numberOfButtons = document.querySelectorAll(".drum").length;
 
 for (i = 0; i<numberOfButtons; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", 
-        function () {
-            alert = "I got clicked";
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+            var buttonInnerHTML = this.innerHTML;
+            
+            switch (buttonInnerHTML) {
+                case "w":
+                    var tom1 = new Audio("./sounds/tom-1.mp3");
+                    tom1.play();
+                    break;
+                case "a":
+                var tom2 = new Audio("./sounds/tom-2.mp3");
+                    tom2.play();
+                    break;
+                case "s":
+                    var tom3 = new Audio("sounds/tom-3.mp3");
+                    tom3.play();
+                    break;
+                case "d":
+                    var tom4 = new Audio("sounds/tom-4.mp3");
+                        tom4.play();
+                        break;
+                case "k":
+                    var snare = new Audio("./sounds/snare.mp3");
+                        snare.play();
+                        break;
+                case "l":
+                    var crash = new Audio("./sounds/crash.mp3");
+                    crash.play();
+                    break;
+                case "j":
+                    var kick = new Audio("./sounds/kick-bass.mp3");
+                    kick.play();
+                    break;
+                default:
+                    console.log(buttonInnerHTML);
+                    break;
+            }
         }
     )
 }
+
